dc685c166bef96151408903807f32c2cdfaeb81c
vranga <vranga@gmail.com>
2018-10-02 17:01:48 +0530

    Added Exercise description from text book at the top of the file

--- Chapter 2: Building Abstractions with Data/2.5  Systems with Generic Operations/Exercise 2.97 reduce rational function.rkt
+++ Chapter 2: Building Abstractions with Data/2.5  Systems with Generic Operations/Exercise 2.97 reduce rational function.rkt
@@ -1,13 +1,35 @@
 #lang racket
 
-; Exercise 2.96.  a. Implement the procedure pseudoremainder-terms, which is just like remainder-terms
-; except that it multiplies the dividend by the integerizing factor described above before calling
-; div-terms. Modify gcd-terms to use pseudoremainder-terms, and verify that greatest-common-divisor
-; now produces an answer with integer coefficients on the example in exercise 2.95.
+; Exercise 2.97.  a. Implement this algorithm as a procedure reduce-terms that takes two term lists n and d
+; as arguments and returns a list nn, dd, which are n and d reduced to lowest terms via the algorithm
+; given above. Also write a procedure reduce-poly, analogous to add-poly, that checks to see if the two
+; polys have the same variable. If so, reduce-poly strips off the variable and passes the problem to
+; reduce-terms, then reattaches the variable to the two term lists supplied by reduce-terms.
 
-; b. The GCD now has integer coefficients, but they are larger than those of P1. Modify gcd-terms
-; so that it removes common factors from the coefficients of the answer by dividing all the
-; coefficients by their (integer) greatest common divisor.
+; b. Define a procedure analogous to reduce-terms that does what the original make-rat did for integers:
+
+; (define (reduce-integers n d)
+;   (let ((g (gcd n d)))
+;     (list (/ n g) (/ d g))))
+
+; and define reduce as a generic operation that calls apply-generic to dispatch to either reduce-poly
+; (for polynomial arguments) or reduce-integers (for scheme-number arguments). You can now easily make
+; the rational-arithmetic package reduce fractions to lowest terms by having make-rat call reduce before
+; combining the given numerator and denominator to form a rational number. The system now handles rational
+; expressions in either integers or polynomials. To test your program, try the example at the beginning of
+; this extended exercise:
+
+; (define p1 (make-polynomial 'x '((1 1)(0 1))))
+; (define p2 (make-polynomial 'x '((3 1)(0 -1))))
+; (define p3 (make-polynomial 'x '((1 1))))
+; (define p4 (make-polynomial 'x '((2 1)(0 -1))))
+
+; (define rf1 (make-rational p1 p2))
+; (define rf2 (make-rational p3 p4))
+
+; (add rf1 rf2)
+
+; See if you get the correct answer, correctly reduced to lowest terms.
 
 ; S O L U T I O N
 
