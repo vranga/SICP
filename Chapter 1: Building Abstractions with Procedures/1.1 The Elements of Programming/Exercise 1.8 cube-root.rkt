f178662bc7996802cce8ed21781677fea2a598c5
vranga <vranga@gmail.com>
2019-01-18 19:00:42 +0530

    Inserted the Question at the top and the test results at the bottom

--- Chapter 1: Building Abstractions with Procedures/1.1 The Elements of Programming/Exercise 1.8 cube-root.rkt
+++ Chapter 1: Building Abstractions with Procedures/1.1 The Elements of Programming/Exercise 1.8 cube-root.rkt
@@ -1,3 +1,14 @@
+#lang racket
+
+; Exercise 1.8.  Newton's method for cube roots is based on the fact that if y is an approximation
+; to the cube root of x, then a better approximation is given by the value
+
+; (x/(y^2) + 2y) / 3
+
+; Use this formula to implement a cube-root procedure analogous to the square-root procedure.
+
+; SOLUTION
+
 (define startingGuess 1.0)
 
 (define (cbrt x)
@@ -24,3 +35,25 @@
 )
 
 (define (square x) (* x x))
+
+; Tests
+
+Welcome to DrRacket, version 6.11 [3m].
+Language: racket, with debugging; memory limit: 512 MB.
+> (cbrt 8)
+2.000000000012062
+> (cbrt 7)
+1.9129320405969417
+> (cbrt 6)
+1.817120681877066
+> (cbrt 5)
+1.709975950782189
+> (cbrt 4)
+1.5874010520152708
+> (cbrt 3)
+1.4422497895989996
+> (cbrt 2)
+1.2599210500177698
+> (cbrt 1)
+1.0
+> 
