a39895b4270329bd33879230f1a163c0d5cc1cdd
vranga <vranga@gmail.com>
2019-08-12 08:31:13 +0530

    Completed and tested

---
+++ Chapter 3: Modularity, Objects and State/3.5 Streams/Exercise 3.65 natural logarithm of 2.rkt
@@ -0,0 +1,350 @@
+#lang racket
+
+; Exercise 3.65.  Use the series
+
+; ln(2) = 1 - 1/2 + 1/3 - 1/4 + ...
+
+; to compute three sequences of approximations to the natural logarithm of 2,
+; in the same way we did above for . How rapidly do these sequences converge?
+
+; S O L U T I O N
+
+; As we can see from the results below, the rate of convergene increases dramatically
+; with euler transforms and tableau. I ran multiple tests on the un-accelerated series.
+; Even with 10000 terms, the value of ln(2) computed was 0.6930971830599583 whereas
+; the desired value is 0.69314718056. The difference is 0.0000499975.
+; Euler acceleration is much faster and gets very close to the desired value in 5000
+; steps. The value after combining 5000 terms is 0.6931471805589525.
+; By using tableau acceleration, we reach 0.6931471805599454 just 10 steps!
+
+(define (partial-sums S)
+	(stream-cons (stream-first S) (add-streams (partial-sums S) (stream-rest S)))
+)
+
+(define (ln2-summands n)
+	(stream-cons
+		(/ 1.0 n)
+		(stream-map - (ln2-summands (+ n 1)))
+	)
+)
+
+(define ln2-stream
+	(partial-sums (ln2-summands 1))
+)
+
+(define (sqrt x tolerance)
+	(stream-limit (sqrt-stream x) tolerance)
+)
+
+(define (stream-limit s t)
+	(let ((s0 (stream-ref s 0)) (s1 (stream-ref s 1)))
+		(if (< (abs (- s1 s0)) t)
+			s1
+			(stream-limit (stream-rest s) t)
+		)
+	)
+)
+
+(define (sqrt-stream x)
+	(define guesses
+		(stream-cons
+			1.0
+			(stream-map
+				(lambda (guess) (sqrt-improve guess x))
+				guesses
+			)
+		)
+	)
+	guesses
+)
+
+(define (sqrt-improve guess x)
+	(average guess (/ x guess))
+)
+
+(define (average x y) (/ (+ x y) 2))
+(define (square x) (* x x))
+
+(define (euler-transform s)
+	(let ((s0 (stream-ref s 0))          ; Sn-1
+		 (s1 (stream-ref s 1))           ; Sn
+		 (s2 (stream-ref s 2)))          ; Sn+1
+		(stream-cons
+			(-
+				s2
+				(/ (square (- s2 s1)) (+ s0 (* -2 s1) s2))
+			)
+			(euler-transform (stream-rest s))
+		)
+	)
+)
+
+(define (make-tableau transform s)
+	(stream-cons
+		s
+		(make-tableau transform (transform s))
+	)
+)
+
+(define (accelerated-sequence transform s)
+	(stream-map stream-first (make-tableau transform s))
+)
+
+(define (scale-stream stream factor)
+	(stream-map (lambda (x) (* x factor)) stream)
+)
+
+(define (stream-map proc . argstreams)
+	; (displayln "Entered stream-map")
+	(if (stream-empty? (car argstreams))
+		empty-stream
+		(stream-cons
+			(apply proc (map stream-first argstreams))
+			(apply stream-map (cons proc (map stream-rest argstreams)))
+		)
+	)
+)
+
+(define (stream-ref s n)
+	; (display "Entered stream-ref with n = ")
+	; (display n)
+	; (newline)
+	(if (= n 0)
+		(stream-first s)
+		(stream-ref (stream-rest s) (- n 1))
+	)
+)
+
+; This procedure displays a finite number of elements from the supplied stream
+; as specified by 'count'
+(define (display-stream-elements count s)
+	(if (= 0 count)
+		(begin
+			(newline)
+			'done
+		)
+		(begin
+			(newline)
+			(display (stream-first s))
+			(display-stream-elements (- count 1) (stream-rest s))
+		)
+	)
+)
+
+(define (display-stream s)
+	(stream-for-each display-line s)
+)
+
+(define (display-line x)
+	(newline)
+	(display x)
+)
+
+(define (div-series dividend-series divisor-series)
+	(cond
+		((= 0 (stream-first divisor-series))
+			(error "Denominator should not have a zero constant: " (stream-first divisor-series))
+		)
+		(else
+			(mul-series
+				dividend-series
+				(invert-unit-series (scale-stream divisor-series (/ 1 (stream-first divisor-series))))
+			)
+		)
+	)
+)
+
+(define (invert-unit-series s)
+	(stream-cons
+		1
+		(mul-series
+			(scale-stream (stream-rest s) -1)
+			(invert-unit-series s)
+		)
+	)
+)
+
+(define (mul-series s1 s2)
+	(stream-cons
+		; (a0 * b0) (The following is the constant term of the series resulting from
+		; the multiplication)
+		(* (stream-first s1) (stream-first s2))
+		; The following is the rest of the series starting with the x^1 term
+		(add-streams
+			; {a0 * (b1x + b2x^2 + b3x^3 + ...)} +
+			; {b0 * (a1x + a2x^2 + a3x^3 + ...)} +
+			(add-streams
+				(scale-stream (stream-rest s2) (stream-first s1))
+				(scale-stream (stream-rest s1) (stream-first s2))
+			)
+			; {(a1x + a2x^2 + a3x^3 + ...) * (b1x + b2x^2 + b3x^3 + ...)}
+			(stream-cons
+				; 0 needs to be prepended to this stream so that the first term of the stream
+				; is the x^1 term. Only then the outer add-streams will add like terms in the
+				; two series supplied to it
+				0
+				(mul-series (stream-rest s1) (stream-rest s2))
+			)
+		)
+	)
+)
+
+(define exp-series
+	(stream-cons 1 (integrate-series exp-series))
+)
+
+; the integral of negative sine is cosine
+(define cosine-series
+	(stream-cons 1 (integrate-series (scale-stream sine-series -1)))
+)
+
+; the integral of cosine is sine
+(define sine-series
+	(stream-cons 0 (integrate-series cosine-series))
+)
+
+(define tan-series 
+	(div-series sine-series cosine-series)
+)
+
+(define (integrate-series s)
+	(div-streams s integers)
+)
+
+(define (add-streams s1 s2)
+	(stream-map + s1 s2)
+)
+
+(define (div-streams s1 s2)
+	(stream-map / s1 s2)
+)
+
+(define ones (stream-cons 1 ones))
+
+(define integers (stream-cons 1 (add-streams ones integers)))
+
+; Test Driver
+
+(define (run-test return-type proc . args)
+
+	(define (print-item-list items first-time?)
+		(cond
+			((not (pair? items)) (void))
+			(else
+				(if (not first-time?)
+					(display ", ")
+					(void)
+				)
+				(print (car items))
+				(print-item-list (cdr items) false)
+			)
+		)
+	)
+
+	(display "Applying ")
+	(display proc)
+	(if (not (null? args))
+		(begin
+			(display " on: ")
+			(print-item-list args true)
+		)
+		(void)
+	)
+	(newline)
+	(let ((result (apply proc args)))
+		(if (not (eq? return-type 'none))
+			(display "Result: ")
+			(void)
+		)
+		(cond
+			((procedure? result) ((result 'print)))
+			; ((eq? return-type 'deque) (print-deque result))
+			((eq? return-type 'none) (void))
+			(else
+				(print result)
+				(newline)
+			)
+		)
+	)
+	(newline)
+)
+
+(define (execution-time proc . args)
+	(define start-time (current-milliseconds))
+	; (display start-time)
+	; (display " ")
+	(apply proc args)
+	(define end-time (current-milliseconds))
+	; (display end-time) 
+	(display "Execution time of ")
+	(display proc)
+	(display ": ")
+	(- end-time start-time)
+)
+
+; Tests
+
+; Test Results
+
+Welcome to DrRacket, version 6.11 [3m].
+Language: racket, with debugging; memory limit: 1024 MB.
+> (display-stream-elements 20 ln2-stream)
+
+1.0
+0.5
+0.8333333333333333
+0.5833333333333333
+0.7833333333333332
+0.6166666666666666
+0.7595238095238095
+0.6345238095238095
+0.7456349206349207
+0.6456349206349207
+0.7365440115440116
+0.6532106782106782
+0.7301337551337552
+0.6587051837051838
+0.7253718503718505
+0.6628718503718505
+0.7216953797836152
+0.6661398242280596
+0.718771403175428
+0.6687714031754279
+'done
+> (display-stream-elements 20 (euler-transform ln2-stream))
+
+0.7
+0.6904761904761905
+0.6944444444444444
+0.6924242424242424
+0.6935897435897436
+0.6928571428571428
+0.6933473389355742
+0.6930033416875522
+0.6932539682539683
+0.6930657506744464
+0.6932106782106783
+0.6930967180967181
+0.6931879423258734
+0.6931137858557215
+0.6931748806748808
+0.6931239512121866
+0.6931668512550866
+0.6931303775344023
+0.693161647077867
+0.6931346368409872
+'done
+> (display-stream-elements 10 (accelerated-sequence euler-transform ln2-stream))
+
+1.0
+0.7
+0.6932773109243697
+0.6931488693329254
+0.6931471960735491
+0.6931471806635636
+0.6931471805604039
+0.6931471805599445
+0.6931471805599427
+0.6931471805599454
+'done
+> 
