66d7d38579e1c92dd40ed3cbc236274f201e1dfc
vranga <vranga@gmail.com>
2019-08-16 17:02:41 +0530

    Completed and tested

---
+++ Chapter 3: Modularity, Objects and State/3.5 Streams/Exercise 3.70 merge-weighted and weighted-pairs.rkt
@@ -0,0 +1,707 @@
+#lang racket
+	
+; Exercise 3.70.  It would be nice to be able to generate streams in which the pairs appear
+; in some useful order, rather than in the order that results from an ad hoc interleaving
+; process. We can use a technique similar to the merge procedure of exercise 3.56, if we
+; define a way to say that one pair of integers is "less than" another. One way to do
+; this is to define a "weighting function" W(i,j) and stipulate that (i1,j1) is less than
+; (i2,j2) if W(i1,j1) < W(i2,j2). Write a procedure merge-weighted that is like merge,
+; except that merge-weighted takes an additional argument weight, which is a procedure
+; that computes the weight of a pair, and is used to determine the order in which elements
+; should appear in the resulting merged stream. Using this, generalize pairs to a
+; procedure weighted-pairs that takes two streams, together with a procedure that computes
+; a weighting function, and generates the stream of pairs, ordered according to weight.
+; Use your procedure to generate
+
+; a. The stream of all pairs of positive integers (i,j) with i < j ordered according to
+; the sum i + j
+
+; b. the stream of all pairs of positive integers (i,j) with i < j, where neither i nor j
+; is divisible by 2, 3, or 5, and the pairs are ordered according to the sum 2i + 3j + 5ij.
+
+; S O L U T I O N
+
+(define (weighted-pairs s t weight)
+	(stream-cons
+		(list (stream-first s) (stream-first t))
+		(merge-weighted
+			(stream-map (lambda (x) (list (stream-first s) x)) (stream-rest t))
+			(weighted-pairs (stream-rest s) (stream-rest t) weight)
+			weight
+		)
+	)
+)
+
+(define (merge-weighted s1 s2 weight)
+	(cond
+		((stream-empty? s1) s2)
+		((stream-empty? s2) s1)
+		(else
+			(let ((s1car (stream-first s1)) (s2car (stream-first s2)))
+				(cond
+					((< (weight s1car) (weight s2car))
+						(stream-cons s1car (merge-weighted (stream-rest s1) s2 weight))
+					)
+					((> (weight s1car) (weight s2car))
+						(stream-cons s2car (merge-weighted s1 (stream-rest s2) weight))
+					)
+					(else
+						(stream-cons
+							s1car
+							(stream-cons
+								s2car
+								(merge-weighted (stream-rest s1) (stream-rest s2) weight)
+							)
+						)
+					)
+				)
+			)
+		)
+	)
+)
+
+; The following procedure expects two ascending infinite streams,
+; one of which needs to be a subset of the other. Let S be the stream with the superset
+; and let T be the stream with the subset. Then this procedure will produce a new stream
+; that contains all those elements that are present in S but not in T
+(define (s-minus-t s t)
+	; Assumes that S and T are ordered ascending and T is a subset of S
+	(cond
+		((stream-empty? t) s)
+		((stream-empty? s) empty-stream)
+		(else
+			(let ((scar (stream-first s)) (tcar (stream-first t)))
+				(cond
+					((< scar tcar)
+						(stream-cons
+							scar
+							(s-minus-t (stream-rest s) t)
+						)
+					)
+					((> scar tcar)
+						(s-minus-t s (stream-rest t))
+					)
+					(else
+						(s-minus-t (stream-rest s) (stream-rest t))
+					)
+				)
+			)
+		)
+	)
+)
+
+; The following proc copied from SICP Exercise 3.56
+(define (merge s1 s2)
+	(cond
+		((stream-empty? s1) s2)
+		((stream-empty? s2) s1)
+		(else
+			(let ((s1car (stream-first s1)) (s2car (stream-first s2)))
+				(cond
+					((< s1car s2car)
+						(stream-cons s1car (merge (stream-rest s1) s2))
+					)
+					((> s1car s2car)
+						(stream-cons s2car (merge s1 (stream-rest s2)))
+					)
+					(else
+						(stream-cons s1car
+							(merge (stream-rest s1) (stream-rest s2))
+						)
+					)
+				)
+			)
+		)
+	)
+)
+
+; Implementation of procedure triples
+; The infinite streams S, T and U are represented as follows:
+
+; S0		T0		U0
+; S1		T1		U1
+; S2		T2		U2
+; S3		T3		U3
+; S4		T4		U4
+; .		.		.
+; .		.		.
+; .		.		.
+
+; All triples (Si, Tj, Uk) such that i < j < k can be produced by combining the following:
+
+; 1.	(S0, T1, U2)
+; 2.	S0 combined with all the pairs produced using the streams (T1, T2, T3, ...) and
+; 		(U1, U2, U3, ...) such that for every pair (Tj, Uk), j < k. We need to exclude the 
+; 		first element from this combined stream because the first element will be
+; 		(S0, T1, U2) which is already accounted for.
+; 3. 	triples called recursively on the streams (S1, S2, ...) (T1, T2, ...) and (U1, U2, ...)
+
+(define (triples s t u)
+	(stream-cons
+		(list
+			(stream-first s)
+			(stream-first (stream-rest t))
+			(stream-first (stream-rest (stream-rest u)))
+		)
+		(interleave
+			(stream-map
+				(lambda (x) (cons (stream-first s) x))
+				(stream-rest (less-than-pairs (stream-rest t) (stream-rest u)))
+			)
+			(triples (stream-rest s) (stream-rest t) (stream-rest u))
+		)
+	)
+)
+
+(define (is-pythagorean-triple? triple)
+	; This procedure assumes that the elements in the triple are in increasing order 
+	(= (+ (square (car triple)) (square (car (cdr triple)))) (square (car (cdr (cdr triple)))))
+)
+
+; This procedures produces all pairs (Si, Tj) where i < j
+(define (less-than-pairs s t)
+	(stream-cons
+		(list (stream-first s) (stream-first (stream-rest t)))
+		(interleave
+			(stream-map (lambda (x) (list (stream-first s) x)) (stream-rest (stream-rest t)))
+			(less-than-pairs (stream-rest s) (stream-rest t))
+		)
+	)
+)
+
+(define (all-pairs s t)
+	(stream-cons
+		(list (stream-first s) (stream-first t))
+		(interleave
+			(stream-map (lambda (x) (list (stream-first s) x)) (stream-rest t))
+			(interleave
+				(stream-map (lambda (x) (list x (stream-first t))) (stream-rest s))
+				(all-pairs (stream-rest s) (stream-rest t))
+			)
+		)
+	)
+)
+
+(define (pairs s t)
+	(stream-cons
+		(list (stream-first s) (stream-first t))
+		(interleave
+			(stream-map (lambda (x) (list (stream-first s) x)) (stream-rest t))
+			(pairs (stream-rest s) (stream-rest t))
+		)
+	)
+)
+
+(define (interleave s1 s2)
+	(if (stream-empty? s1)
+		s2
+		(stream-cons
+			(stream-first s1)
+			(interleave s2 (stream-rest s1))
+		)
+	)
+)
+
+(define (partial-sums S)
+	(stream-cons (stream-first S) (add-streams (partial-sums S) (stream-rest S)))
+)
+
+(define (ln2-summands n)
+	(stream-cons
+		(/ 1.0 n)
+		(stream-map - (ln2-summands (+ n 1)))
+	)
+)
+
+(define ln2-stream
+	(partial-sums (ln2-summands 1))
+)
+
+(define (sqrt x tolerance)
+	(stream-limit (sqrt-stream x) tolerance)
+)
+
+(define (stream-limit s t)
+	(let ((s0 (stream-ref s 0)) (s1 (stream-ref s 1)))
+			(if (< (abs (- s1 s0)) t)
+			s1
+			(stream-limit (stream-rest s) t)
+		)
+	)
+)
+
+(define (sqrt-stream x)
+	(define guesses
+		(stream-cons
+			1.0
+			(stream-map
+				(lambda (guess) (sqrt-improve guess x))
+				guesses
+			)
+		)
+	)
+	guesses
+)
+
+(define (sqrt-improve guess x)
+	(average guess (/ x guess))
+)
+
+(define (average x y) (/ (+ x y) 2))
+(define (square x) (* x x))
+
+(define (euler-transform s)
+	(let ((s0 (stream-ref s 0))          ; Sn-1
+		 (s1 (stream-ref s 1))           ; Sn
+		 (s2 (stream-ref s 2)))          ; Sn+1
+		(stream-cons
+			(-
+				s2
+				(/ (square (- s2 s1)) (+ s0 (* -2 s1) s2))
+			)
+			(euler-transform (stream-rest s))
+		)
+	)
+)
+
+(define (make-tableau transform s)
+	(stream-cons
+		s
+		(make-tableau transform (transform s))
+	)
+)
+
+(define (accelerated-sequence transform s)
+	(stream-map stream-first (make-tableau transform s))
+)
+
+(define (scale-stream stream factor)
+	(stream-map (lambda (x) (* x factor)) stream)
+)
+
+(define (stream-map proc . argstreams)
+	; (displayln "Entered stream-map")
+	(if (stream-empty? (car argstreams))
+		empty-stream
+		(stream-cons
+			(apply proc (map stream-first argstreams))
+			(apply stream-map (cons proc (map stream-rest argstreams)))
+		)
+	)
+)
+
+(define (stream-filter pred stream)
+	(cond
+		((stream-empty? stream) empty-stream)
+		((pred (stream-first stream))
+			(stream-cons
+				(stream-first stream)
+				(stream-filter pred (stream-rest stream))
+			)
+		)
+		(else
+			(stream-filter pred (stream-rest stream))
+		)
+	)
+)
+
+(define (stream-ref s n)
+	; (display "Entered stream-ref with n = ")
+	; (display n)
+	; (newline)
+	(if (= n 0)
+		(stream-first s)
+		(stream-ref (stream-rest s) (- n 1))
+	)
+)
+
+; This procedure produces a stream with a maximum number of count elements from
+; the supplied stream
+(define (truncate-stream s count)
+	(if (<= count 0)
+		empty-stream
+		(stream-cons
+			(stream-first s)
+			(truncate-stream (stream-rest s) (- count 1))
+		)
+	)
+)
+
+; This procedure displays a finite number of elements from the supplied stream
+; as specified by 'count'
+(define (display-stream-elements count s)
+	(if (= 0 count)
+		(begin
+			(newline)
+			'done
+		)
+		(begin
+			(newline)
+			(display (stream-first s))
+			(display-stream-elements (- count 1) (stream-rest s))
+		)
+	)
+)
+
+(define (display-stream s)
+	(stream-for-each display-line s)
+)
+
+(define (display-line x)
+	(newline)
+	(display x)
+)
+
+(define (indent-and-display pair)
+	(display-spaces (car pair))
+	(display pair)
+	(newline)
+)
+
+(define (display-spaces n)
+	(if (<= n 0)
+		(void)
+		(begin
+			(display " ")
+			(display-spaces (- n 1))
+		)
+	)
+)
+
+(define (div-series dividend-series divisor-series)
+	(cond
+		((= 0 (stream-first divisor-series))
+			(error "Denominator should not have a zero constant: " (stream-first divisor-series))
+		)
+		(else
+			(mul-series
+				dividend-series
+				(invert-unit-series (scale-stream divisor-series (/ 1 (stream-first divisor-series))))
+			)
+		)
+	)
+)
+
+(define (invert-unit-series s)
+	(stream-cons
+		1
+		(mul-series
+			(scale-stream (stream-rest s) -1)
+			(invert-unit-series s)
+		)
+	)
+)
+
+(define (mul-series s1 s2)
+	(stream-cons
+		; (a0 * b0) (The following is the constant term of the series resulting from
+		; the multiplication)
+		(* (stream-first s1) (stream-first s2))
+		; The following is the rest of the series starting with the x^1 term
+		(add-streams
+			; {a0 * (b1x + b2x^2 + b3x^3 + ...)} +
+			; {b0 * (a1x + a2x^2 + a3x^3 + ...)} +
+			(add-streams
+				(scale-stream (stream-rest s2) (stream-first s1))
+				(scale-stream (stream-rest s1) (stream-first s2))
+			)
+			; {(a1x + a2x^2 + a3x^3 + ...) * (b1x + b2x^2 + b3x^3 + ...)}
+			(stream-cons
+				; 0 needs to be prepended to this stream so that the first term of the stream
+				; is the x^1 term. Only then the outer add-streams will add like terms in the
+				; two series supplied to it
+				0
+				(mul-series (stream-rest s1) (stream-rest s2))
+			)
+		)
+	)
+)
+
+(define exp-series
+	(stream-cons 1 (integrate-series exp-series))
+)
+
+; the integral of negative sine is cosine
+(define cosine-series
+	(stream-cons 1 (integrate-series (scale-stream sine-series -1)))
+)
+
+; the integral of cosine is sine
+(define sine-series
+	(stream-cons 0 (integrate-series cosine-series))
+)
+
+(define tan-series 
+	(div-series sine-series cosine-series)
+)
+
+(define (integrate-series s)
+	(div-streams s integers)
+)
+
+(define (add-streams s1 s2)
+	(stream-map + s1 s2)
+)
+
+(define (div-streams s1 s2)
+	(stream-map / s1 s2)
+)
+
+(define ones (stream-cons 1 ones))
+
+(define integers (stream-cons 1 (add-streams ones integers)))
+
+; Test Driver
+
+(define (run-test return-type proc . args)
+
+	(define (print-item-list items first-time?)
+		(cond
+			((not (pair? items)) (void))
+			(else
+				(if (not first-time?)
+					(display ", ")
+					(void)
+				)
+				(print (car items))
+				(print-item-list (cdr items) false)
+			)
+		)
+	)
+
+	(display "Applying ")
+	(display proc)
+	(if (not (null? args))
+		(begin
+			(display " on: ")
+			(print-item-list args true)
+		)
+		(void)
+	)
+	(newline)
+	(let ((result (apply proc args)))
+		(if (not (eq? return-type 'none))
+			(display "Result: ")
+			(void)
+		)
+		(cond
+			((procedure? result) ((result 'print)))
+			; ((eq? return-type 'deque) (print-deque result))
+			((eq? return-type 'none) (void))
+			(else
+				(print result)
+				(newline)
+			)
+		)
+	)
+	(newline)
+)
+
+(define (execution-time proc . args)
+	(define start-time (current-milliseconds))
+	; (display start-time)
+	; (display " ")
+	(apply proc args)
+	(define end-time (current-milliseconds))
+	; (display end-time) 
+	(display "Execution time of ")
+	(display proc)
+	(display ": ")
+	(- end-time start-time)
+)
+
+; Tests
+
+; Test Results
+
+Welcome to DrRacket, version 6.11 [3m].
+Language: racket, with debugging; memory limit: 4096 MB.
+> ; Part a
+
+(define (pair-weight-as-sum pair)
+	(+ (car pair) (cadr pair))
+)
+
+> (display-stream-elements 85 (weighted-pairs integers integers pair-weight-as-sum))
+
+(1 1)
+(1 2)
+(1 3)
+(2 2)
+(1 4)
+(2 3)
+(1 5)
+(2 4)
+(3 3)
+(1 6)
+(2 5)
+(3 4)
+(1 7)
+(2 6)
+(3 5)
+(4 4)
+(1 8)
+(2 7)
+(3 6)
+(4 5)
+(1 9)
+(2 8)
+(3 7)
+(4 6)
+(5 5)
+(1 10)
+(2 9)
+(3 8)
+(4 7)
+(5 6)
+(1 11)
+(2 10)
+(3 9)
+(4 8)
+(5 7)
+(6 6)
+(1 12)
+(2 11)
+(3 10)
+(4 9)
+(5 8)
+(6 7)
+(1 13)
+(2 12)
+(3 11)
+(4 10)
+(5 9)
+(6 8)
+(7 7)
+(1 14)
+(2 13)
+(3 12)
+(4 11)
+(5 10)
+(6 9)
+(7 8)
+(1 15)
+(2 14)
+(3 13)
+(4 12)
+(5 11)
+(6 10)
+(7 9)
+(8 8)
+(1 16)
+(2 15)
+(3 14)
+(4 13)
+(5 12)
+(6 11)
+(7 10)
+(8 9)
+(1 17)
+(2 16)
+(3 15)
+(4 14)
+(5 13)
+(6 12)
+(7 11)
+(8 10)
+(9 9)
+(1 18)
+(2 17)
+(3 16)
+(4 15)
+'done
+> ; Part b
+(define (pair-weight-custom pair)
+	(+ (* 2 (car pair)) (* 3 (cadr pair)) (* 5 (car pair) (cadr pair)))
+)
+> (define integer-stream-not-multiple-of-2-3-5
+	(s-minus-t
+		(s-minus-t
+			(s-minus-t integers (scale-stream integers 2))
+			(scale-stream integers 3)
+		)
+		(scale-stream integers 5)
+	)
+)
+> (display-stream-elements 20 integer-stream-not-multiple-of-2-3-5)
+
+1
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+49
+53
+59
+61
+67
+71
+73
+'done
+> (display-stream-elements 50
+	(weighted-pairs
+		integer-stream-not-multiple-of-2-3-5
+		integer-stream-not-multiple-of-2-3-5
+		pair-weight-custom
+	)
+)
+
+(1 1)
+(1 7)
+(1 11)
+(1 13)
+(1 17)
+(1 19)
+(1 23)
+(1 29)
+(1 31)
+(7 7)
+(1 37)
+(1 41)
+(1 43)
+(1 47)
+(1 49)
+(1 53)
+(7 11)
+(1 59)
+(1 61)
+(7 13)
+(1 67)
+(1 71)
+(1 73)
+(1 77)
+(1 79)
+(7 17)
+(11 11)
+(1 83)
+(1 89)
+(1 91)
+(7 19)
+(11 13)
+(1 97)
+(1 101)
+(1 103)
+(1 107)
+(1 109)
+(7 23)
+(1 113)
+(13 13)
+(1 119)
+(1 121)
+(11 17)
+(1 127)
+(1 131)
+(1 133)
+(1 137)
+(1 139)
+(7 29)
+(11 19)
+'done
