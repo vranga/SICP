4228cf455b395225357965957a2909a430df7c2f
vranga <vranga@gmail.com>
2019-09-03 13:44:07 +0530

    Completed

--- Chapter 4: Metalinguistic Abstraction/The Metacircular Evaluator/Exercise 4.2 Louis Reasoner eval.rkt
+++ Chapter 4: Metalinguistic Abstraction/The Metacircular Evaluator/Exercise 4.2 Louis Reasoner eval.rkt
@@ -15,7 +15,296 @@
 ; applications start with call. For example, instead of (factorial 3) we will now have to
 ; write (call factorial 3) and instead of (+ 1 2) we will have to write (call + 1 2).
 
+; E X P L A N A T I O N
+; a. Louis Reasoner's implementation with the reordered cond clauses will cause infinite
+; loops. When (define x 3) is evaluated, it will be interpreted as a procedure application
+; so eval will be called on it again causing an infinite loop.
+;
+
 ; S O L U T I O N
+
+(define (eval exp env)
+	(cond
+		((self-evaluating? exp) exp)
+		((variable? exp) (lookup-variable-value exp env))
+		((quoted? exp) (text-of-quotation exp))
+		((application? exp)
+			(apply
+				(eval (operator exp) env)
+				(list-of-values (operands exp) env)
+			)
+		)
+		((assignment? exp) (eval-assignment exp env))
+		((definition? exp) (eval-definition exp env))
+		((if? exp) (eval-if exp env))
+		((lambda? exp)
+			(make-procedure
+				(lambda-parameters exp)
+				(lambda-body exp)
+				env
+			)
+		)
+		((begin? exp) (eval-sequence (begin-actions exp) env))
+		((cond? exp) (eval (cond->if exp) env))
+		(else
+			(error "Unknown expression type -- EVAL" exp)
+		)
+	)
+)
+
+(define (self-evaluating? exp)
+	(cond
+		((number? exp) true)
+		((string? exp) true)
+		(else false)
+	)
+)
+
+(define (quoted? exp)
+	(tagged-list? exp 'quote)
+)
+
+(define (text-of-quotation exp) (cadr exp))
+
+(define (assignment? exp)
+	(tagged-list? exp 'set!)
+)
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+(define (eval-assignment exp env)
+	(set-variable-value!
+		(assignment-variable exp)
+		(eval (assignment-value exp) env)
+		env
+	)
+	'ok
+)
+
+(define (definition? exp)
+	(tagged-list? exp 'define)
+)
+
+(define (definition-variable exp)
+	(if (symbol? (cadr exp))
+		(cadr exp)
+		(caadr exp)
+	)
+)
+
+(define (definition-value exp)
+	(if (symbol? (cadr exp))
+		(caddr exp)
+		(make-lambda
+			(cdadr exp)		; formal parameters
+			(cddr exp)		; body
+		)
+	)
+)
+
+(define (eval-definition exp env)
+	(define-variable!
+		(definition-variable exp)
+		(eval (definition-value exp) env)
+		env
+	)
+	'ok
+)
+
+(define (variable? exp) (symbol? exp))
+
+(define (lookup-variable-value var env)
+	(define (env-loop env)
+		(define (scan vars vals)
+			(cond
+				((null? vars)
+					(env-loop (enclosing-environment env))
+				)
+				((eq? var (car vars))
+					(car vals)
+				)
+				(else
+					(scan (cdr vars) (cdr vals))
+				)
+			)
+		)
+		(if (eq? env the-empty-environment)
+			(error "Unbound variable" var)
+			(let ((frame (first-frame env)))
+				(scan
+					(frame-variables frame)
+					(frame-values frame)
+				)
+			)
+		)
+	)
+	(env-loop env)
+)
+
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+	(if (not (null? (cdddr exp)))
+		(cadddr exp)
+		'false
+	)
+)
+(define (make-if predicate consequent alternative)
+	(list 'if predicate consequent alternative)
+)
+
+(define (eval-if exp env)
+	(if (true? (eval (if-predicate exp) env))
+		(eval (if-consequent exp) env)
+		(eval (if-alternative exp) env)
+	)
+)
+
+(define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+(define (make-lambda parameters body)
+	(cons 'lambda (cons parameters body))
+)
+
+(define (make-procedure parameters body env)
+	(list 'procedure parameters body env)
+)
+
+(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (eval-sequence exps env)
+	(cond
+		((last-exp? exps) (eval (first-exp exps) env))
+		(else (eval (first-exp exps) env)
+			(eval-sequence (rest-exps exps) env)
+		)
+	)
+)
+(define (sequence->exp seq)
+	(cond
+		((null? seq) seq)
+		((last-exp? seq) (first-exp seq))
+		(else (make-begin seq))
+	)
+)
+(define (make-begin seq) (cons 'begin seq))
+
+
+
+(define (cond? exp) (tagged-list? exp 'cond))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-else-clause? clause)
+	(eq? (cond-predicate clause) 'else)
+)
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if exp)
+	(expand-clauses (cond-clauses exp))
+)
+
+(define (expand-clauses clauses)
+	(if (null? clauses)
+		'false ; no else clause
+		(let ((first (car clauses)) (rest (cdr clauses)))
+			(if (cond-else-clause? first)
+				(if (null? rest)
+					(sequence->exp (cond-actions first))
+					(error "ELSE clause isn't last -- COND->IF" clauses)
+				)
+				(make-if
+					(cond-predicate first)
+					(sequence->exp (cond-actions first))
+					(expand-clauses rest)
+				)
+			)
+		)
+	)
+)
+
+(define (application? exp) (tagged-list? exp 'call))
+(define (operator exp) (cadr exp))
+(define (operands exp) (cddr exp))
+
+(define (list-of-values exps env)
+	(evaluate-l-to-r-list-of-values exps env)
+)
+
+(define (tagged-list? exp tag)
+	(if (pair? exp)
+		(eq? (car exp) tag)
+		false
+	)
+)
+
+(define (define-variable! var val env)
+	(let ((frame (first-frame env)))
+		(define (scan vars vals)
+			(cond
+				((null? vars)
+					(add-binding-to-frame! var val frame)
+				)
+				((eq? var (car vars))
+					(set-mcar! vals val)
+				)
+				(else (scan (cdr vars) (cdr vals)))
+			)
+		)
+		(scan
+			(frame-variables frame)
+			(frame-values frame)
+		)
+	)
+)
+
+(define (set-variable-value! var val env)
+	(define (env-loop env)
+		(define (scan vars vals)
+			(cond
+				((null? vars)
+					(env-loop (enclosing-environment env))
+				)
+				((eq? var (car vars))
+					(set-mcar! vals val)
+				)
+				(else (scan (cdr vars) (cdr vals)))
+			)
+		)
+		(if (eq? env the-empty-environment)
+			(error "Unbound variable -- SET!" var)
+			(let ((frame (first-frame env)))
+				(scan
+					(frame-variables frame)
+					(frame-values frame)
+				)
+			)
+		)
+	)
+	(env-loop env)
+)
+
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+	(set-mcar! frame (cons var (car frame)))
+	(set-mcdr! frame (cons val (cdr frame)))
+)
+
+(define (true? x) (not (eq? x false)))
+(define (false? x) (eq? x false))
 
 (define (evaluate-l-to-r-list-of-values exps env)
 	(if (no-operands? exps)
@@ -39,29 +328,6 @@
 			)
 		)
 	)
-)
-
-; Dummy implementation (to allow compilation of the above procedures)
-(define (no-operands? exps)
-	(if (pair? exps)
-		false
-		true
-	)
-)
-
-(define (first-operand exps)
-	(car exps)
-)
-
-(define (rest-operands exps)
-	(cdr exps)
-)
-
-(define (eval exp env)
-	(display "Evaluating ")
-	(display exp)
-	(newline)
-	exp
 )
 
 ; Test Driver
@@ -126,33 +392,3 @@
 ; Tests
 
 ; Test Results
-
-Welcome to DrRacket, version 6.11 [3m].
-Language: racket, with debugging; memory limit: 4096 MB.
-> (define expressions (list 'a 'b 'c 'd 'e 'f 'g 'h 'i 'j))
-> (define env 'env)
-> (evaluate-l-to-r-list-of-values expressions env)
-Evaluating a
-Evaluating b
-Evaluating c
-Evaluating d
-Evaluating e
-Evaluating f
-Evaluating g
-Evaluating h
-Evaluating i
-Evaluating j
-'(a b c d e f g h i j)
-> (evaluate-r-to-l-list-of-values expressions env)
-Evaluating j
-Evaluating i
-Evaluating h
-Evaluating g
-Evaluating f
-Evaluating e
-Evaluating d
-Evaluating c
-Evaluating b
-Evaluating a
-'(a b c d e f g h i j)
-> 
