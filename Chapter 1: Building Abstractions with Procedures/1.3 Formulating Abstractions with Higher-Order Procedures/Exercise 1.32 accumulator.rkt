125c4ce4aaef442b3e852d43ea96af86bad313f6
vranga <vranga@gmail.com>
2019-09-04 06:36:14 +0530

    Pasted the SICP question text at the top

--- Chapter 1: Building Abstractions with Procedures/1.3 Formulating Abstractions with Higher-Order Procedures/Exercise 1.32 accumulator.rkt
+++ Chapter 1: Building Abstractions with Procedures/1.3 Formulating Abstractions with Higher-Order Procedures/Exercise 1.32 accumulator.rkt
@@ -1,3 +1,21 @@
+; Exercise 1.32.  a. Show that sum and product (exercise 1.31) are both special cases of a
+; still more general notion called accumulate that combines a collection of terms, using
+; some general accumulation function:
+
+; (accumulate combiner null-value term a next b)
+
+; Accumulate takes as arguments the same term and range specifications as sum and product,
+; together with a combiner procedure (of two arguments) that specifies how the current term
+; is to be combined with the accumulation of the preceding terms and a null-value that
+; specifies what base value to use when the terms run out. Write accumulate and show how sum
+; and product can both be defined as simple calls to accumulate.
+
+; b. If your accumulate procedure generates a recursive process, write one that generates
+; an iterative process. If it generates an iterative process, write one that generates a
+; recursive process.
+
+; SOLUTION
+
 (define (accumulate-recur combiner null-value term a next b)
 	(if
 		(> a b) null-value
